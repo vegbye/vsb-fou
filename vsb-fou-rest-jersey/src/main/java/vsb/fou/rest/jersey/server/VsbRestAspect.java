67900b3873a59bf98ffc6b0d93491c8403b0c065
vegbye <vegbye@gmail.com>
2013-09-19 13:44:17 +0200

    FoU: Jersey REST

--- vsb-fou-rest-jersey/src/main/java/vsb/fou/rest/jersey/server/VsbRestAspect.java
+++ vsb-fou-rest-jersey/src/main/java/vsb/fou/rest/jersey/server/VsbRestAspect.java
@@ -1,11 +1,60 @@
 package vsb.fou.rest.jersey.server;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
 
 @Aspect
 @Component
 public class VsbRestAspect {
 
+    @Pointcut("within(@javax.jws.WebService *)")
+    public void webServiceAnnotatedClass() {
+    }
+
+    @Pointcut("within(@sb1.lip.infra.ws.WebServiceWithoutMetadata *)")
+    public void webServiceWithoutMetadataAnnotatedClass() {
+    }
+
+    @Pointcut("execution(public * *(..))")
+    public void publicMethod() {
+    }
+
+    @Pointcut("execution(@javax.annotation.Resource * *(..))")
+    public void publicSpringSetter() {
+    }
+
+    /**
+     * Dette gjøres før og etter (rundt eller around) Prccess.execute, men etter beforeExecute
+     */
+    @Around("publicMethod() && webServiceAnnotatedClass() && !webServiceWithoutMetadataAnnotatedClass() && !publicSpringSetter()")
+    public Object aroundExeute(ProceedingJoinPoint jp) throws Throwable {
+        Object request = getRequest(jp);
+        MethodSignature signature = (MethodSignature) jp.getSignature();
+        Class<?> webServiceClass = jp.getTarget().getClass();
+        Method method = findMethod(webServiceClass, signature.getName());
+
+        lipServiceHandler.before(method, request);
+        lipServiceAudit.startPerformance("WS");
+        lipServiceAudit.logTransaction(TransactionType.REQUEST, request);
+        Object jaxbResponse = null;
+        try {
+            jaxbResponse = lipExecutorService.submit(createLipCallable(jp));
+        } catch (ErrorCodeException e) {
+            jaxbResponse = lipServiceErrorAudit.afterThrowing(e, e.getErrorCode());
+        } catch (Exception e) {
+            jaxbResponse = lipServiceErrorAudit.afterThrowing(e, InfraErrorCode.UNEXPECTED_ERROR_3);
+        } finally {
+            lipServiceAudit.logTransaction(TransactionType.RESPONSE, jaxbResponse);
+            lipServiceAudit.stopPerformance();
+            lipServiceHandler.after();
+        }
+        return jaxbResponse;
+    }
 
 }
