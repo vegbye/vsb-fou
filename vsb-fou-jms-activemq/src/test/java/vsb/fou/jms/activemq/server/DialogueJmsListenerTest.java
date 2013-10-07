01af66ce33f4e725f439bf5f6291abaf05ae4147
vegbye <vegbye@gmail.com>
2013-10-07 14:59:02 +0200

    FoU: JMS ActiveMQ

---
+++ vsb-fou-jms-activemq/src/test/java/vsb/fou/jms/activemq/server/DialogueJmsListenerTest.java
@@ -0,0 +1,38 @@
+package vsb.fou.jms.activemq.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
+
+import javax.jms.Message;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+/**
+ * @author Vegard S. Bye
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class DialogueJmsListenerTest {
+
+    @InjectMocks
+    private DialogueJmsListener dialogueJmsListener;
+    @Mock
+    private JmsTemplate jmsTemplate;
+
+    @Test
+    public void testIt() {
+        Message message = Mockito.mock(Message.class);
+        dialogueJmsListener.onMessage(message);
+
+        verify(jmsTemplate).send(anyString(), any(MessageCreator.class));
+        verifyNoMoreInteractions(jmsTemplate);
+    }
+}
