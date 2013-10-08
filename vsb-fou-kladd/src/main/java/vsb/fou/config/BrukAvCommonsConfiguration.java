1229b07f8163d1f43bc147fb14012a4016643222
vegbye <vegbye@gmail.com>
2013-10-08 20:08:10 +0200

    FoU: JMS ActiveMQ

---
+++ vsb-fou-kladd/src/main/java/vsb/fou/config/BrukAvCommonsConfiguration.java
@@ -0,0 +1,26 @@
+package vsb.fou.config;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.commons.configuration.reloading.FileChangedReloadingStrategy;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+
+public class BrukAvCommonsConfiguration {
+
+    public static void main(String[] args) throws ConfigurationException, IOException {
+        new BrukAvCommonsConfiguration().loadPropsFile();
+    }
+
+    public void loadPropsFile() throws ConfigurationException, IOException {
+        PropertiesConfiguration config = new PropertiesConfiguration(new ClassPathResource("vsb.properties").getFile());
+        FileChangedReloadingStrategy strategy = new FileChangedReloadingStrategy();
+        strategy.setRefreshDelay(5 * 60 * 1000L);
+        config.setReloadingStrategy(strategy);
+        String kaffe = config.getString("vsb.kaffe");
+        System.out.println("kaffe = " + kaffe);
+        double versjon = config.getDouble("versjon");
+        System.out.println("versjon = " + versjon);
+    }
+}
