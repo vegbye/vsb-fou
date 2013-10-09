6301a7ed6bfa79ebdb3e9b7049f9b4461ad1d0f9
vegbye <vegbye@gmail.com>
2013-10-09 10:18:52 +0200

    FoU: JMS ActiveMQ

--- vsb-fou-jms-activemq/src/main/java/vsb/fou/jms/activemq/client/common/JmsKonstanter.java
+++ vsb-fou-jms-activemq/src/main/java/vsb/fou/jms/activemq/client/common/JmsKonstanter.java
@@ -8,7 +8,6 @@ public final class JmsKonstanter {
     public static final String ASYNC_REQUEST_QUEUE = "asyncRequestQueue";
     public static final String SYNC_REQUEST_QUEUE = "syncRequestQueue";
     public static final String SYNC_REPLY_QUEUE = "syncReplyQueue";
-    public static final String BROKER_NAME = "vsb-fou-broker";
 
     private JmsKonstanter() {
     }
