c26d337f719e9598612c69bf8d248b9c13517fe4
vegbye <vegbye@gmail.com>
2013-10-09 10:55:33 +0200

    FoU: Database H2

--- vsb-fou-db-h2/src/main/java/vsb/fou/jms/activemq/client/producer/MainCtxActiveMqProducer.java
+++
@@ -1,9 +0,0 @@
-package vsb.fou.jms.activemq.client.producer;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("vsb.fou.jms.activemq.client.producer")
-public class MainCtxActiveMqProducer {
-}
