062851d2444774b40b0a92c417f7c40f8f049709
vegbye <vegbye@gmail.com>
2013-10-14 08:33:39 +0200

    FoU: Database H2

---
+++ vsb-fou-ws-cxf/src/main/java/vsb/fou/ws/cxf/server/VsbFouWsCxfWebAppInit.java
@@ -0,0 +1,17 @@
+package vsb.fou.ws.cxf.server;
+
+//import org.springframework.web.WebApplicationInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+/**
+ * @author Vegard S. Bye
+ */
+public class VsbFouWsCxfWebAppInit /*implements WebApplicationInitializer*/ {
+
+    //@Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
