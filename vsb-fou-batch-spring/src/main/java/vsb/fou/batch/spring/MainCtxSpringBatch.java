04f4b30dd7d8954dff492389dbfe0258ed361701
vegbye <vegbye@gmail.com>
2013-10-16 16:52:39 +0200

    FoU: Spring Batch

--- vsb-fou-batch-spring/src/main/java/vsb/fou/batch/spring/MainCtxSpringBatch.java
+++ vsb-fou-batch-spring/src/main/java/vsb/fou/batch/spring/MainCtxSpringBatch.java
@@ -4,12 +4,14 @@ import org.springframework.batch.admin.web.resources.DefaultResourceService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  * @author Vegard S. Bye
  */
 @Configuration
 @ComponentScan("vsb.fou.batch.spring")
+@ImportResource({"classpath:/spring-batch-admin.xml"})
 public class MainCtxSpringBatch {
 
     @Bean
