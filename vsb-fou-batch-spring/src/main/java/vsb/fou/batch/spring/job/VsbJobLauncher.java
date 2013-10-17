e08a9ff6456ec7269bdfe138713625e8f8b4d3b7
vegbye <vegbye@gmail.com>
2013-10-17 20:05:18 +0200

    FoU: Spring Batch

---
+++ vsb-fou-batch-spring/src/main/java/vsb/fou/batch/spring/job/VsbJobLauncher.java
@@ -0,0 +1,16 @@
+package vsb.fou.batch.spring.job;
+
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+/**
+ * @author Vegard S. Bye
+ */
+public class VsbJobLauncher extends QuartzJobBean {
+
+    @Override
+    protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
