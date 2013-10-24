ddc1eda2473fe8a49fc4aeca5959271a2f71d7d2
vegbye <vegbye@gmail.com>
2013-10-24 20:57:15 +0200

    FoU: Spring Batch + Quartz

--- vsb-fou-batch-spring/src/main/java/vsb/fou/batch/spring/quartz/MainCtxSpringBatchQuartz.java
+++ vsb-fou-batch-spring/src/main/java/vsb/fou/batch/spring/quartz/MainCtxSpringBatchQuartz.java
@@ -3,12 +3,13 @@ package vsb.fou.batch.spring.quartz;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import vsb.fou.common.InfraConfig;
 
 /**
  * @author Vegard S. Bye
  */
 @Configuration
-@ComponentScan("vsb.fou.batch.spring.quartz")
+@ComponentScan(basePackages = "vsb.fou.batch.spring.quartz", excludeFilters = {@ComponentScan.Filter(InfraConfig.class)})
 @Import({JobDetailCtx.class, CronTriggerCtx.class})
 public class MainCtxSpringBatchQuartz {
 }
--- vsb-fou-batch-spring/src/main/java/vsb/fou/batch/spring/web/AppCtxSpringBatchWeb.java
+++ vsb-fou-batch-spring/src/main/java/vsb/fou/batch/spring/web/AppCtxSpringBatchWeb.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import vsb.fou.batch.spring.infra.InfraCtxSpringBatch;
 import vsb.fou.batch.spring.job.MainCtxImportProductJob;
+import vsb.fou.batch.spring.quartz.InfraCtxSpringBatchQuartz;
 import vsb.fou.batch.spring.quartz.MainCtxSpringBatchQuartz;
 import vsb.fou.batch.spring.springbatchadmin.MainCtxSpringBatchAdmin;
 import vsb.fou.common.InfraConfig;
@@ -16,6 +17,7 @@ import vsb.fou.common.InfraConfig;
         MainCtxImportProductJob.class,
         InfraCtxSpringBatch.class,
         MainCtxSpringBatchWeb.class,
+        InfraCtxSpringBatchQuartz.class,
         MainCtxSpringBatchQuartz.class,
         MainCtxSpringBatchAdmin.class
 })
